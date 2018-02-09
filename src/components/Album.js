648c5824b9bb6228dc523d356c5ea1c1a485f744
wktdev <wktdev@gmail.com>
2018-02-08 22:15:31 -0800

    completed components library checkpoint

---
+++ src/components/Album.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+
+class Album extends Component {
+   render() {
+     return (
+       <section className="album">
+         {this.props.match.params.slug} Album will go here 
+       </section>
+     );
+   }
+ }
+
+ export default Album
